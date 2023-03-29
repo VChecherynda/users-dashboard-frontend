85bd83333a60734bff8d30f931ea6abbb44e5ba6
vadym <checherinda.vadym@webbylab.com.com>
2023-03-30 02:16:42 +0300

    Fix route problems

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from "next/head";
 
-import Chat from './components/chat'
+import Chat from './chat'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Chat/>
+      <Component {...pageProps} />
     </>
   );
 }
